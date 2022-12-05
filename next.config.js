b535524d6084ec1f2fcae6a6446ed337bfc9aea6
tsots <mosalatsotetsi@gmail.com>
2022-12-05 16:57:52 +0200

    load sass

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 const { prefix } = "./utils/prefix";
 
 const nextConfig = {
   reactStrictMode: true,
   basePath: prefix,
   assetPrefix: prefix,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
 };
 
 module.exports = nextConfig;
